5717d4dc6fe3187cb0e2061d2204f5503e5e40ab
shimamurasaki <saki.1130.rm.mk@gmail.com>
2024-04-28 03:43:54 +0900

    0427

--- src/js/script.js
+++ src/js/script.js
@@ -13,12 +13,34 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
   });
 
-  //スワイパー
-    let swiper = new Swiper(".js-mv-swiper", {
+  //mvスワイパー
+    let mvSwiper = new Swiper(".js-mv-swiper", {
       loop: true,
       autoplay: {
       delay: 3000,
       },
+    });
+
+    //campaignスワイパー
+    let campaignSwiper = new Swiper(".js-campaign-swiper", {
+      loop: true,
+      slidesPerView: 1.2,// 2枚目20％だけ表示
+      breakpoints: {
+        // スライドの表示枚数：600px以上の場合
+        600: {
+          slidesPerView: 2.2,
+        },
+        // スライドの表示枚数：768px以上の場合
+        768: {
+          slidesPerView: 3.3,
+        },
+      },
+      grabCursor: true, // カーソルを置いたときに指のカーソルを表示
+      navigation: {
+        nextEl: ".js-campaign-button-next",
+        prevEl: ".js-campaign-button-prev",
+      },
+      clickable: true,
     });
 
 
