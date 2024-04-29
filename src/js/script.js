0e6400add4a13aa01882e3d6309e241ace95eb50
shimamurasaki <saki.1130.rm.mk@gmail.com>
2024-04-29 15:25:52 +0900

    0429

--- src/js/script.js
+++ src/js/script.js
@@ -43,5 +43,44 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       clickable: true,
     });
 
+    //背景色の後に画像やテキストが表示されるエフェクト
+    //要素の取得とスピードの設定
+      var box = $('.colorbox'),
+      speed = 700;  
+
+      //.colorboxの付いた全ての要素に対して下記の処理を行う
+      box.each(function(){
+        $(this).append('<div class="color"></div>')
+        var color = $(this).find($('.color')),
+        image = $(this).find('img');
+        var counter = 0;
+
+        image.css('opacity','0');
+        color.css('width','0%');
+        //inviewを使って背景色が画面に現れたら処理をする
+        color.on('inview', function(){
+          if(counter == 0){
+            $(this).delay(200).animate({'width':'100%'},speed,function(){
+              image.css('opacity','1');
+              $(this).css({'left':'0' , 'right':'auto'});
+              $(this).animate({'width':'0%'},speed);
+            })
+            counter = 1;
+          }
+        });
+      });
+    
+
+      // ローディング画面
+      $(function () {
+        function end_loader() {
+          $('.loader').fadeOut(800);
+        }
+        $(window).on('load', function () {
+          setTimeout(function () {
+            end_loader();
+          }, 3000)
+        })
+      })
 
 });
