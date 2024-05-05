d202b979e603e0ba860e19f0a201a29d776d6fa1
shimamurasaki <saki.1130.rm.mk@gmail.com>
2024-05-06 02:35:13 +0900

    テキストFB修正完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,31 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
   //ドロワーメニュー
+  $(document).ready(function() {
+    // ウィンドウの幅をチェックしてドロワーメニューを制御
+    function checkWidth() {
+      var windowWidth = $(window).width();
+      if (windowWidth >= 768) { // 幅が768px以上の場合（タブレット以上）
+        $('.js-hamburger').removeClass('is-active');
+        $(".js-sp-nav").hide();
+      }
+    }
+  
+    // 初期読み込み時とウィンドウのリサイズ時にチェックを実行
+    checkWidth();
+    $(window).resize(checkWidth);
+  });
+  
   $(".js-hamburger").click(function () {
     if ($('.js-hamburger').hasClass('is-active')) {
       $('.js-hamburger').removeClass("is-active");
-      // $("html").toggleClass("is-fixed");
       $(".js-sp-nav").fadeOut(300);
     } else {
       $('.js-hamburger').addClass("is-active");
-      // $("html").toggleClass("is-fixed");
       $(".js-sp-nav").fadeIn(300);
     }
   });
-
+  
   $(".js-sp-title, .js-sp-text").click(function () {
     if ($('.js-hamburger').hasClass('is-active')) {
         $('.js-hamburger').removeClass("is-active");
@@ -22,6 +35,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         $(".js-sp-nav").fadeIn(300);
     }
   });
+  
 
   //mvスワイパー
     let mvSwiper = new Swiper(".js-mv-swiper", {
@@ -35,16 +49,6 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     let campaignSwiper = new Swiper(".js-campaign-swiper", {
       loop: true,
       slidesPerView: 1.2,// 2枚目20％だけ表示
-      breakpoints: {
-        // スライドの表示枚数：600px以上の場合
-        600: {
-          slidesPerView: 2.2,
-        },
-        // スライドの表示枚数：768px以上の場合
-        768: {
-          slidesPerView: 3.3,
-        },
-      },
       grabCursor: true, // カーソルを置いたときに指のカーソルを表示
       navigation: {
         nextEl: ".js-campaign-button-next",
